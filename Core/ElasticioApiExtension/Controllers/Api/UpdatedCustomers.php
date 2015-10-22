8357901d64adb38461a181292b10d83f17dc2c51
ellenaua <elenasharovar@gmail.com>
2015-10-22 12:10:09 +0300

    Updated customers endpoint

--- Core/ElasticioApiExtension/Controllers/Api/UpdatedCustomers.php
+++ Core/ElasticioApiExtension/Controllers/Api/UpdatedCustomers.php
@@ -9,27 +9,38 @@ class Shopware_Controllers_Api_UpdatedCustomers extends Shopware_Controllers_Api
         $this->resource = \Shopware\Components\Api\Manager::getResource('updatedCustomer');
     }
 
+    private function findCustomerIds($updatedSince, $offset, $limit)
+    {
+        $sql = "SELECT id FROM s_user "
+             . "WHERE UNIX_TIMESTAMP(updatedOn) > {$updatedSince} "
+             . "ORDER BY updatedOn ASC " // from oldest to newest
+             . "LIMIT {$offset}, {$limit} ";
+
+        $statement = Shopware()->Db()->query($sql);
+        $ids = $statement->fetchAll(\PDO::FETCH_COLUMN);
+        return $ids;
+    }
+
     /**
      * GET /api/updatedCustomers/
+     * GET /api/updatedCustomers?since=5345345343344
      */
     public function indexAction()
     {
-        $sql = "SELECT id FROM s_user WHERE UNIX_TIMESTAMP(updatedOn) > 0 ";
-        $statement = Shopware()->Db()->query($sql);
-        $ids = $statement->fetchAll(\PDO::FETCH_COLUMN);
-        error_log(print_r($ids, true));
-
         $limit  = $this->Request()->getParam('limit', 1000);
         $offset = $this->Request()->getParam('start', 0);
         $sort   = $this->Request()->getParam('sort', array());
         $filter = $this->Request()->getParam('filter', array());
 
-        array_push($filter, array(
-            'property'   => 'id'
-            'value'      => $ids
-        ));
+        $updatedSince = $this->Request()->getParam('updatedSince', 0);
 
-        $result = $this->resource->getList($offset, $limit, $filter, $sort);
+        if (!$updatedSince) {
+            $result = $this->resource->getList($offset, $limit, $filter, $sort);
+        } else {
+            $customerIds = $this->findCustomerIds($updatedSince, $offset, $limit);
+            array_push($filter, array('property'   => 'id', 'value' => $customerIds));
+            $result = $this->resource->getList(0, $limit, $filter, $sort);
+        }
 
         $this->View()->assign($result);
         $this->View()->assign('success', true);
